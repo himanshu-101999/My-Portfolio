35f3a2130a30fc46376c5697f90739e54b7245a9
unknown <himanshurajvis12@gmail.com>
2024-11-19 14:24:49 +0530

    modifide  about section

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("",views.home),   
+    path("about/",views.about),
+    path("contact/",views.contect),
+    path("skills/",views.skills),
+    path("project/",views.project),
+    path("certificate/",views.certificate),
 ]
--- base/views.py
+++ base/views.py
@@ -3,3 +3,18 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def contect(request):
+    return render(request,'contect.html')
+
+def skills(request):
+    return render(request,'skills.html')
+
+def project(request):
+    return render(request,'project.html')
+
+def certificate(request):
+    return render(request,'certificate.html')
